3fc825f211efcc27b422212d4ca4e89bf7c8d1ce
maylenevong <104463482+maylenevong@users.noreply.github.com>
2022-04-28 13:59:24 -0700

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ Created on Wed Jan 29 10:29:55 2020
 
 #insert local parameters
 
-username = 'your-spotify-username'
-client_id ='your-client-id'
-client_secret = 'your-client-secret'
+username = 'kkmvkkmv'
+client_id ='122225f903954913a8a1e9717f58a348'
+client_secret = '1b17bf6e8fd046a9a7f06fa70ccffb69'
 redirect_uri = 'http://localhost:7777/callback'
 scope = 'user-read-recently-played'
